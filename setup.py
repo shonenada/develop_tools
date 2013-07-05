1ee9597d11dbc49f1d03a7682dbd65cf3a342124
shonenada <shonenada@gmail.com>
2013-07-05 16:50:20 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+metadata = {"name": "develop_tools",
+            "version": "0.1",
+            "author": "shonenada",
+            "author_email": "shonenada@gmail.com",
+            "url": "https://github.com/shonenada/develop_tools/",
+            "platforms": "any",
+            "packages" = find_packages(),
+            "scripts": ["tools.py"],
+            "description": "A tools set to help develop."}
+
+
+if __name__ == "__main__":
+    setup(**metadata)
